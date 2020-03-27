e308deb06de011174c5b8038802db49c136f1722
AgenderAmethyst <62758645+AgenderAmethyst@users.noreply.github.com>
2020-03-27 15:47:55 +0000

    mimes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var paths = {};
 function getFolderContents(folder, callback) {
     var rFolder = new XMLHttpRequest();
     rFolder.open("GET", folder, true);
+    rFolder.overrideMimeType("text/html")
     rFolder.send(null);
     var ret = rFolder.responseText;
     return ret.split('\n');
